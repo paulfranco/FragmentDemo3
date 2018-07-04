7cc8807e27d575bfbbdacd725e6a4636b403cf35
Paul Franco <paulfranco@me.com>
2018-07-03 21:42:10 -0700

    Pass data from the activity to a fragment using the Bundle Object

--- app/src/main/java/co/paulfran/paulfranco/fragmentdemo3/FragmentA.java
+++ app/src/main/java/co/paulfran/paulfranco/fragmentdemo3/FragmentA.java
@@ -22,7 +22,12 @@ public class FragmentA extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, container, false);
-        Log.i(TAG, "onCreateView()");
+
+        // Get the reference to the incoming Bundle Object
+        Bundle bundle = getArguments();
+        // use a default value in case the method fails to find the associated with the key to prevent the app from crashing (Prevent an null pointer exception)
+        final int firstNum = bundle.getInt(Constants.FIRST_NUM, 0);
+        final int secondNum = bundle.getInt(Constants.SECOND_NUM, 0);
 
         // Complete initialization of the button and text view
         // Inside the FragmentA.java findViewBYId() method does not exists. for that purpose we need to make use of the ( view )
@@ -32,18 +37,19 @@ public class FragmentA extends Fragment {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // 
-
-
+                // Code to add the Two Numbers passed in
+                addTwoNumbers(firstNum, secondNum);
             }
         });
-
-
 
         return view;
     }
 
+    private void addTwoNumbers(int firstNum, int secondNum) {
+
+        int result = firstNum + secondNum;
+        txvResult.setText("Result: " + result);
+    }
 
 
 }
--- app/src/main/java/co/paulfran/paulfranco/fragmentdemo3/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/fragmentdemo3/MainActivity.java
@@ -29,11 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendDataToFragment(View view) {
 
+        // Extract Data passed by the user in the etFirstNumber and etSecondNumber EditTexts and convert to an integer
+        int firstNumber = Integer.valueOf(etFirstNumber.getText().toString());
+        int secondNumber = Integer.valueOf(etSecondNumber.getText().toString());
+
+        // Define bundle object
+        // The Bundle Object stores a value inside of it as a key/value pair
+        Bundle bundle = new Bundle();
+        bundle.putInt(Constants.FIRST_NUM, firstNumber);
+        bundle.putInt(Constants.SECOND_NUM, secondNumber);
+
         // Add FragmentA into the inside the container layout containerFragmentA
-        FragmentA framentA = new FragmentA();
+        FragmentA fragmentA = new FragmentA();
+
+        // The objective is to pass the bundle Object to the FragmentA for that purpose we need to make use of the method setArguments()
+        // which takes the parameters of bundle objects
+        fragmentA.setArguments(bundle);
+
 
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.containerFragmentA, framentA, "fragA");
+        transaction.add(R.id.containerFragmentA, fragmentA, "fragA");
         transaction.commit();
 
     }
